7c8a5c5e39852871ea49dbceb9de6442e37777b0
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-03-05 13:45:55 +0530

    File updated at 3/5/2026, 1:45:56 PM

--- 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.cpp
+++ 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.cpp
@@ -1,15 +1,11 @@
-// Last updated: 3/5/2026, 1:40:22 PM
+// Last updated: 3/5/2026, 1:45:56 PM
 1class Solution {
 2public:
-3    int minOperations(string& s) {
-4        //op[0] count op need for "0101.." 
-5        //op[1] count op need for "1010.."
-6        int n=s.size(), op[2]={0};
-7        for(int i=0; i<n; i+=2){
-8            op[s[i]=='0']++;
-9            if (i+1<n) [[likely]]
-10                op[s[i+1]=='1']++;
-11        }
-12        return min(op[0], op[1]);
-13    }
-14};
+3    int minOperations(string s) {
+4        int n, c = 0, j = 0;
+5        for(n = 0; n < s.size(); n++, j ^= 1) {
+6            if(s[n] - '0' == j) c++;
+7        }
+8        return min(c, n - c);
+9    }
+10};
