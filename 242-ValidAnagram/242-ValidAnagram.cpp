85818c10218a08469fbec53309c67901913c8b0f
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-03-19 19:31:17 +0530

    File updated at 3/19/2026, 7:31:16 PM

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,4 +1,4 @@
-// Last updated: 3/19/2026, 7:30:14 PM
+// Last updated: 3/19/2026, 7:31:16 PM
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
@@ -9,19 +9,15 @@
 8        sort(s.begin(),s.end());
 9        sort(t.begin(),t.end());
 10
-11        bool res = false;
+11        bool res = true;
 12        for(int i = 0;i<s.size();i++)
 13        {
-14            if(s[i]==t[i])
+14            if(s[i]!=t[i])
 15            {
-16                res = true;
+16             return false;
 17            }
-18            else
-19            {
-20                return false;
-21            }
-22            cout<<res;
-23        }
-24        return res;
-25    }
-26};
+18   
+19        }
+20        return res;
+21    }
+22};
