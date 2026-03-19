73c74bcefe729c1b6a00a372678e54611699eb7a
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-03-19 19:36:35 +0530

    File updated at 3/19/2026, 7:36:35 PM

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,23 +1,24 @@
-// Last updated: 3/19/2026, 7:31:16 PM
+// Last updated: 3/19/2026, 7:36:35 PM
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4                if(s.size()!=t.size())
-5        {
-6            return false;
-7        }
-8        sort(s.begin(),s.end());
-9        sort(t.begin(),t.end());
-10
-11        bool res = true;
-12        for(int i = 0;i<s.size();i++)
-13        {
-14            if(s[i]!=t[i])
-15            {
-16             return false;
-17            }
-18   
+4        // In O(N) using frequency method
+5        if(s.size()!=t.size())
+6        {
+7            return false;
+8        }
+9          vector<int> count(26,0);
+10        for(int i = 0;i<s.size();i++)
+11        {
+12            count[s[i]-'a']++;
+13            count[t[i]-'a']--;
+14        }
+15      
+16        for(int i = 0;i<26;i++)
+17        {
+18            if(count[i]!=0) return false;
 19        }
-20        return res;
-21    }
-22};
+20
+21        return true;
+22    }
+23};
