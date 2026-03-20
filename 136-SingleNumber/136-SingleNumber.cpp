dd1eccbf93b4093f8492c6b48100b207762a064d
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-03-21 00:58:22 +0530

    File updated at 3/21/2026, 12:58:22 AM

--- 136-SingleNumber/136-SingleNumber.cpp
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -1,13 +1,12 @@
-// Last updated: 3/21/2026, 12:58:07 AM
+// Last updated: 3/21/2026, 12:58:22 AM
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
-5        int ans = 0;
-6        for(int i=0;i<nums.size();i++)
-7        {
-8            ans ^= nums[i];
-9        }
-10        return ans;
-11    }
-12};
+4        int ans = 0;
+5        for(int i=0;i<nums.size();i++)
+6        {
+7            ans ^= nums[i];
+8        }
+9        return ans;
+10    }
+11};
