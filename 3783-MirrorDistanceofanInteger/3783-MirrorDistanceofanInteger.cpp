25b329ec2ed6866d10d7d8a22430bf3bb7e52b70
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-04-18 22:32:32 +0530

    File updated at 4/18/2026, 10:32:40 PM

--- 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.cpp
+++ 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.cpp
@@ -1,10 +1,18 @@
-// Last updated: 4/18/2026, 10:27:30 PM
+// Last updated: 4/18/2026, 10:32:40 PM
 1class Solution {
 2public:
 3    int mirrorDistance(int n) {
-4        string s = to_string(n);
-5        reverse(s.begin(),s.end());
-6        int flip = stoi(s);
-7        return abs(n-flip);
-8    }
-9};
+4        
+5        // int flip = 0;
+6        int rem = n;
+7        string flip = "";
+8        while(rem!=0)
+9        {
+10            int d = rem%10;
+11            flip += to_string(d);
+12            rem /=10;
+13        }
+14        int flip2 = stoi(flip);
+15        return abs(n-flip2);
+16    }
+17};
