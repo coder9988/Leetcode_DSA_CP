e7206cdcc3724f104ae9c59a4969e51f9d49fbf2
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-04-26 17:13:09 +0530

    File updated at 4/26/2026, 5:13:15 PM

--- 1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.cpp
+++ 1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.cpp
@@ -1,58 +1,38 @@
-// Last updated: 4/26/2026, 5:12:39 PM
-1class Solution {
-2public:
-3    bool bfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-4        int n = grid.size();
-5        int m = grid[0].size();
-6
-7        queue<pair<pair<int,int>, pair<int,int>>> q;
-8        q.push({{i,j}, {-1,-1}});
-9        visited[i][j] = true;
-10
-11        int dirs[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-12
-13        while(!q.empty()) {
-14            auto curr = q.front(); q.pop();
-15
-16            int x = curr.first.first;
-17            int y = curr.first.second;
-18            int px = curr.second.first;
-19            int py = curr.second.second;
-20
-21            for(auto &d : dirs) {
-22                int nx = x + d[0];
-23                int ny = y + d[1];
-24
-25                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-26
-27                if(grid[nx][ny] != grid[x][y]) continue;
-28
-29                if(nx == px && ny == py) continue;
-30
-31                if(visited[nx][ny]) return true;
-32
-33                visited[nx][ny] = true;
-34                q.push({{nx, ny}, {x, y}});
-35            }
-36        }
-37        return false;
-38    }
-39
-40    bool containsCycle(vector<vector<char>>& grid) {
-41        int n = grid.size();
-42        int m = grid[0].size();
-43
-44        vector<vector<bool>> visited(n, vector<bool>(m, false));
-45
-46        for(int i = 0; i < n; i++) {
-47            for(int j = 0; j < m; j++) {
-48                if(!visited[i][j]) {
-49                    if(bfs(i, j, grid, visited)) {
-50                        return true;
-51                    }
-52                }
-53            }
-54        }
-55        return false;
-56    }
-57};
+// Last updated: 4/26/2026, 5:13:15 PM
+1int dir[5]={0, 1, 0, -1, 0};
+2int r, c;
+3class Solution {
+4public:
+5    static inline bool isOutside(int i, int j, int r, int c){
+6        return i<0||i>=r||j<0||j>=c;
+7    }
+8    static bool dfs(int i, int j, int i0, int j0, int alpha, vector<vector<char>>& grid){
+9        grid[i][j]=-alpha;
+10        for(int a=0; a<4; a++){
+11            const int s=i+dir[a], t=j+dir[a+1];
+12            if (isOutside(s, t, r, c) || (s==i0 && t==j0)) continue;
+13            if (grid[s][t]==-alpha) return 1;
+14            if (grid[s][t]==alpha){
+15                if (dfs(s, t, i, j, alpha, grid)) return 1;
+16            }
+17        }
+18        return 0;
+19    }
+20    static bool containsCycle(vector<vector<char>>& grid) {
+21        r=grid.size(), c=grid[0].size();
+22        for(int i=0; i<r; i++){
+23            for(int j=0; j<c; j++){
+24                if (grid[i][j]<0) continue;
+25                if (dfs(i, j, -1, -1, grid[i][j], grid)) return 1;
+26            }
+27        }
+28        return 0;
+29    }
+30};
+31
+32auto init = []() {
+33    ios::sync_with_stdio(0);
+34    cin.tie(0);
+35    cout.tie(0);
+36    return 'c';
+37}();
