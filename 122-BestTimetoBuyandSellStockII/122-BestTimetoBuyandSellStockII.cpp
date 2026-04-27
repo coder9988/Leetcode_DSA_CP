bcbd01150d88b425e738eaa726c72dfada12eb86
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-04-27 15:31:58 +0530

    File updated at 4/27/2026, 3:32:04 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
@@ -1,29 +1,17 @@
-// Last updated: 4/27/2026, 2:16:52 PM
+// Last updated: 4/27/2026, 3:32:04 PM
 1class Solution {
 2public:
 3    int maxProfit(vector<int>& prices) {
-4        int maxprofit = 0;
-5        int buy = 0;
-6        int sell = 1;
-7        if(prices.size()==1)
-8        {
-9            return 0;
-10        }
-11        while(sell<prices.size())
-12        {
-13            int profit = prices[sell]-prices[buy];
-14            if(profit<0)
-15            {
-16                buy = sell;
-17                sell+=1;
-18            }
-19            else
-20            {
-21                maxprofit += profit;
-22                buy += 1;
-23                sell += 1;
-24            }
-25        }
-26        return maxprofit;
-27    }
-28};
+4        int profit = 0;
+5
+6        for(int i = 1; i < prices.size(); i++)
+7        {
+8            if(prices[i] > prices[i-1])
+9            {
+10                profit += prices[i] - prices[i-1];
+11            }
+12        }
+13
+14        return profit;
+15    }
+16};
