241ea652c2a0c0da782c885fc4f047251ed57838
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-05-02 23:11:24 +0530

    File updated at 5/2/2026, 11:11:34 PM

--- 788-RotatedDigits/788-RotatedDigits.cpp
+++ 788-RotatedDigits/788-RotatedDigits.cpp
@@ -1,12 +1,12 @@
-// Last updated: 5/2/2026, 11:11:12 PM
+// Last updated: 5/2/2026, 11:11:34 PM
 1class Solution {
 2public:
 3    bool isGood(int n) {
 4        int f = false, s = false;
 5        while(n > 0) {
 6            int ld = n % 10;
-7            if(ld == 3 || ld == 4 || ld == 7) f = true; // these don't give a valid digit after rotating so if these exists (f becomes true) we returb false
-8            if(ld == 2 || ld == 5 || ld == 6 || ld == 9) s = true; // these rotate and give a valid digit so if atleast any one exists it can give us a different number so we return true if s is true only when f is false
+7            if(ld == 3 || ld == 4 || ld == 7) f = true; 
+8            if(ld == 2 || ld == 5 || ld == 6 || ld == 9) s = true; 
 9            n /= 10;
 10        }
 11        if(f) return false;
