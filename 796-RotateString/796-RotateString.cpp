5f06b5c025ce3db986e423b0418c59a4f0f2b618
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-05-03 13:24:11 +0530

    Create 796-RotateString

---
+++ 796-RotateString/796-RotateString.cpp
@@ -0,0 +1,31 @@
+// Last updated: 5/3/2026, 1:24:22 PM
+1class Solution {
+2public:
+3    bool rotateString(string s, string goal) {
+4        int i = 0;
+5        int n = s.size()-1;
+6        if(s.size()==1)
+7        {
+8            return (s==goal)?true:false;
+9        }
+10        while(i<n)
+11        {
+12            if(s==goal)
+13            {
+14                return true;
+15            }
+16            for(int j = 0;j<n;j++)
+17            {
+18                char ch = s[0];
+19                s.erase(0,1);
+20                s+=ch;
+21                            if(s==goal)
+22            {
+23                return true;
+24            }
+25            }
+26            i++;
+27        }
+28        return false;
+29    }
+30};
