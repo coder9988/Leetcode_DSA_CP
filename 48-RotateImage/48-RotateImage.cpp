27a65235541544f39de8f95009c1f08ca9062b4e
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-05-04 19:10:32 +0530

    File updated at 5/4/2026, 7:10:43 PM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,19 +1,17 @@
-// Last updated: 5/4/2026, 6:44:48 PM
+// Last updated: 5/4/2026, 7:10:43 PM
 1class Solution {
 2public:
 3    void rotate(vector<vector<int>>& matrix) {
 4        int n = matrix.size();
-5
-6        // Step 1: transpose
-7        for(int i = 0; i < n; i++){
-8            for(int j = i + 1; j < n; j++){
-9                swap(matrix[i][j], matrix[j][i]);
-10            }
-11        }
-12
-13        // Step 2: reverse each row
-14        for(int i = 0; i < n; i++){
-15            reverse(matrix[i].begin(), matrix[i].end());
-16        }
-17    }
-18};
+5        for(int i = 0; i < n; i++){
+6            for(int j = i + 1; j < n; j++){
+7                swap(matrix[i][j], matrix[j][i]);
+8            }
+9        }
+10
+11        // Step 2: reverse each row
+12        for(int i = 0; i < n; i++){
+13            reverse(matrix[i].begin(), matrix[i].end());
+14        }
+15    }
+16};
