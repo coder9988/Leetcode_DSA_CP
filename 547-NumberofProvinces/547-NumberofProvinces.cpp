8f05f8872e5f9542adc47be82b09a58408e99bd0
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-07-10 13:42:16 +0530

    File updated at 7/10/2026, 1:42:24 PM

--- 547-NumberofProvinces/547-NumberofProvinces.cpp
+++ 547-NumberofProvinces/547-NumberofProvinces.cpp
@@ -1,30 +1,26 @@
-// Last updated: 6/30/2026, 2:19:56 AM
+// Last updated: 7/10/2026, 1:42:24 PM
 1class Solution {
 2public:
-3    void dfs(vector<bool> &visited,vector<vector<int>>& isConnected,int city)
-4    {
-5        visited[city] = true;
-6        for(int i = 0;i<isConnected.size();i++)
-7        {
-8            if(isConnected[city][i] == 1 && !visited[i])
-9            {
-10                visited[city] = true;
-11                dfs(visited,isConnected,i);
-12            }
-13        }
-14    }
-15    int findCircleNum(vector<vector<int>>& isConnected) {
+3    void dfs(vector<vector<int>>& isConnected, vector<bool>& vis, int city) {
+4        vis[city] = true;
+5        for (int i = 0; i < isConnected.size(); i++) {
+6            if (isConnected[city][i] == 1 && !vis[i]) {
+7                vis[i] = true;
+8                dfs(isConnected, vis, i);
+9            }
+10        }
+11    }
+12    int findCircleNum(vector<vector<int>>& isConnected) {
+13
+14        int s = isConnected.size();
+15        vector<bool> visited(s, false);
 16        int province = 0;
-17        int n = isConnected.size();
-18        vector<bool> visited(n,false);
-19        for(int i = 0;i<n;i++)
-20        {
-21            if(!visited[i])
-22            {
-23                dfs(visited,isConnected,i);
-24                province++;
-25            }
-26        }
-27        return province;
-28    }
-29};
+17        for (int i = 0; i < s; i++) {
+18            if (!visited[i]) {
+19                dfs(isConnected, visited, i);
+20                province += 1;
+21            }
+22        }
+23        return province;
+24    }
+25};
