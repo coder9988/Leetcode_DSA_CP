3db3223d47548c51ff91f98da28889c422c89fd1
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-07-26 01:18:11 +0530

    Create 3536-MaximumProductofTwoDigits

---
+++ 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.py
@@ -0,0 +1,15 @@
+# Last updated: 7/26/2026, 1:18:19 AM
+1class Solution(object):
+2    def maxProduct(self, n):
+3        """
+4        :type n: int
+5        :rtype: int
+6        """
+7        l = []
+8        while n!=0:
+9            l.append(n%10)
+10            n/=10
+11        l.sort(reverse=True)
+12        return l[0]*l[1]
+13
+14        
