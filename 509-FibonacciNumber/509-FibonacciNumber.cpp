74fc6d8a1acb9e331678160e4e81de078aa95fca
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-08-02 17:24:55 +0530

    File updated at 8/2/2026, 5:25:00 PM

--- 509-FibonacciNumber/509-FibonacciNumber.cpp
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -1,11 +1,25 @@
-// Last updated: 4/19/2026, 6:00:44 PM
+// Last updated: 8/2/2026, 5:25:00 PM
 1class Solution {
 2public:
-3    int fib(int n) {
-4        if(n==0 || n==1)
-5        {
-6            return n;
-7        }
-8        return fib(n-1) + fib(n-2);
-9    }
-10};
+3    int solve(int n,vector<int> &dp)
+4    {
+5        if(n ==1 || n == 0)
+6        {
+7            return n;
+8        }
+9        if(n<0)
+10        {
+11            return 0;
+12        }
+13        if(dp[n]!=-1)
+14        {
+15            return dp[n];
+16        }
+17        dp[n] = solve(n-1,dp) + solve(n-2,dp);
+18        return dp[n];
+19    }
+20    int fib(int n) {
+21        vector<int> dp(n+1,-1);
+22        return solve(n,dp);
+23    }
+24};
