aafed1aa1fdd7759c6b129b5cfe4c198ae191eb7
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-08-02 18:54:21 +0530

    File updated at 8/2/2026, 6:54:26 PM

--- 509-FibonacciNumber/509-FibonacciNumber.cpp
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -1,25 +1,33 @@
-// Last updated: 8/2/2026, 5:25:00 PM
+// Last updated: 8/2/2026, 6:54:26 PM
 1class Solution {
 2public:
-3    int solve(int n,vector<int> &dp)
-4    {
-5        if(n ==1 || n == 0)
-6        {
-7            return n;
-8        }
-9        if(n<0)
-10        {
-11            return 0;
-12        }
-13        if(dp[n]!=-1)
-14        {
-15            return dp[n];
-16        }
-17        dp[n] = solve(n-1,dp) + solve(n-2,dp);
-18        return dp[n];
-19    }
-20    int fib(int n) {
-21        vector<int> dp(n+1,-1);
-22        return solve(n,dp);
-23    }
-24};
+3const int INF = 1e9;
+4    int solve(int amount,int i,vector<vector<int>> &dp, vector<int>& coins)
+5    {
+6        if(amount == 0)
+7        {
+8            return 0;
+9        }
+10        if(i==coins.size())
+11        {
+12            return INF;
+13        }
+14        if(dp[i][amount]!=-1)
+15        {
+16            return dp[i][amount];
+17        }
+18        int take = INF;
+19        if(coins[i]<=amount)
+20        {
+21            take = 1 + solve(amount-coins[i],i,dp,coins);
+22        }
+23        int notTake = solve(amount,i+1,dp,coins);
+24        dp[i][amount] = min(take,notTake);
+25        return dp[i][amount];
+26    }
+27    int coinChange(vector<int>& coins, int amount) {
+28        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+29        int ans = solve(amount,0,dp,coins);
+30        return (ans==INF)?-1:ans;
+31    }
+32};
