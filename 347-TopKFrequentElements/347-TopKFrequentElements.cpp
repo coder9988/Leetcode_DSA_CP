0e68cf083ec8c0f309999164ff46a148951096e5
Mudit Jain <145495878+coder9988@users.noreply.github.com>
2026-08-29 19:02:40 +0530

    File updated at 8/29/2026, 7:02:50 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,23 +1,30 @@
-// Last updated: 6/18/2026, 5:06:26 PM
+// Last updated: 8/29/2026, 7:02:50 PM
 1class Solution {
 2public:
 3    vector<int> topKFrequent(vector<int>& nums, int k) {
-4        vector<pair<int,int>> v;
-5        map<int,int> freq;
-6        vector<int> res;
-7        for(int i:nums)
-8        {
-9            freq[i]++;
-10        }
-11        for(auto it:freq)
-12        {
-13            v.push_back({it.second,it.first});
-14        } 
-15        sort(v.begin(),v.end(),greater<pair<int,int>>());
-16        for(int i =0;i<k;i++)
-17        {
-18            res.push_back(v[i].second);
-19        }
-20        return res;
-21    }
-22};
+4        map<int,int> mp;
+5        for(int i:nums)
+6        {
+7            mp[i]++;
+8        }
+9        vector<pair<int,int>> pr;
+10        for(auto &i:mp)
+11        {
+12            pr.push_back({i.first,i.second});
+13        }
+14        sort(pr.begin(),pr.end(),[](auto &a,auto &b)
+15        {
+16            if(a.second == b.second)
+17            {
+18                return a.first<b.first;
+19            }
+20            return a.second>b.second;
+21        });
+22        vector<int> ans;
+23        for(int i = 0;i<k;i++)
+24        {
+25            ans.push_back(pr[i].first);
+26        }
+27        return ans;
+28    }
+29};
